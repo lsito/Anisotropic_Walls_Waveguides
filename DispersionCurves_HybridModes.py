ecbcbeab10547a8a769f524f4054049df1c2a528
Leonardo Sito <45729468+lsito@users.noreply.github.com>
2024-01-19 14:02:47 +0100

    Test of dispersion curves

--- DispersionCurves_HybridModes.py
+++ DispersionCurves_HybridModes.py
@@ -27,7 +27,7 @@ b = 10.16e-3 # Short side of waveguide
 zz = 0
 zt = 0
 
-#%% Dispersion relation m = 0
+#%% Dispersion relation m = 0, the mode is TE
 def f_of_kc_me0(kc, freq):
 
     omega = 2*np.pi*freq
@@ -38,7 +38,7 @@ def f_of_kc_me0(kc, freq):
 
     return f
 
-#%% Dispersion relation m != 0
+#%% Dispersion relation m != 0 we do not know if the mode is TE, TM or Hybrid
 def f_of_kc_mne0(kc, m, freq):
 
         omega = 2*np.pi*freq
@@ -53,52 +53,36 @@ def f_of_kc_mne0(kc, m, freq):
         K0xc = k0*kx/kc**2
         Kzyc = gamma*ky/kc**2
 
-        f = np.abs(
-            2*Kzyc**2*zz*(-zt+(X+1)/(1-X)*K0xc*(zz*zt+1)-K0xc**2*zz)
-            + (Kzyc**4+K0xc**4)*zz**2-2*K0xc**3*(X+1)/(1-X)*zz*(zz*zt+1)
-            + K0xc**2*(1+(zz*zt)**2+4*((X+1)/(1-X))**2*zz*zt)
-            - 2*K0xc*(X+1)/(1-X)*zt*(1+zz*zt)
-            + zt**2
-            )
+        if np.abs(zt)*np.abs(zz)==0: # The metal waveguide condition
+            f = np.abs(1-X)
+        else:
+            f = np.abs(
+                2*Kzyc**2*zz*(-zt+(X+1)/(1-X)*K0xc*(zz*zt+1)-K0xc**2*zz)
+                + (Kzyc**4+K0xc**4)*zz**2-2*K0xc**3*(X+1)/(1-X)*zz*(zz*zt+1)
+                + K0xc**2*(1+(zz*zt)**2+4*((X+1)/(1-X))**2*zz*zt)
+                - 2*K0xc*(X+1)/(1-X)*zt*(1+zz*zt)
+                + zt**2
+                )
 
         return f
 
-#%% Compute alpha of hybrid modes
-# Only once we have kc we can get alhpa coupling
-"""
-def compute_alpha(a, b, zz, zt, m, n, freq):
-    
-        Z0 = np.sqrt(sc.mu_0/sc.epsilon_0)
-        omega = 2*np.pi*freq
-        k0 = omega*np.sqrt(sc.epsilon_0*sc.mu_0)
-
-        ky = m*np.pi/b
-        kx = np.sqrt(kc**2-ky**2, dtype = complex)
-        gamma = np.sqrt(kc**2-k0**2, dtype = complex)
-
-        X = np.exp(-1j*2*kx*a, dtype = complex)
-        
-        K0xc = k0*kx/kc**2
-        Kzyc = gamma*ky/kc**2
-        
-        if zt == 0:
-            alpha = -Kzyc/K0xc
-        else:
-            alpha = (K0xc*Kzyc*(zz-1/zt))/(-2*(X+1)/(1-X)*K0xc+zt-zz*Kzyc**2+K0xc**2/zt)/Z0
-            
-        return alpha
-"""
 #%% Root finding of the dispersion equation and support variables
+
+# Note: here "n" is the nth zero of f(kc), then we need a way to call the modes
+
 def compute_kc(a, b, zz, zt, m, n, freq):
     if m == 0:
-        x = np.linspace(1, 1000, 10000)
+        x = np.linspace(1, 1000, 10000) # Hard coded, not nice!
         roots_idx = find_peaks(-f_of_kc_me0(x, freq))[0]
+        
+        roots = x[roots_idx]
+        kc_ = roots[n-1] # Because there is no TEM
     else:
-        x = np.linspace(1, 600, 20000)
+        x = np.linspace(1, 600, 20000) # Hard coded, not nice!
         roots_idx = find_peaks(-f_of_kc_mne0(x, m, freq))[0]
-    
-    roots = x[roots_idx]
-    kc_ = roots[n-1]
+        
+        roots = x[roots_idx]
+        kc_ = roots[n]
     
     k0_ = 2*np.pi*freq*np.sqrt(sc.epsilon_0*sc.mu_0)
     gamma_ = np.sqrt(kc_**2 - k0_**2, dtype=complex)
@@ -107,29 +91,47 @@ def compute_kc(a, b, zz, zt, m, n, freq):
     return k0_, kc_, gamma_, eps_eff_
 
   
-#%% Test Dispersion curves
-m = 0
-n = 1 # Mode number
+#%% Test Dispersion curves with analytical eq.
 
-freq = np.geomspace(1e6, 2e10, 1001)
-k0 = np.zeros_like(freq)
-kc = np.zeros_like(freq)
-gamma = np.zeros_like(freq, dtype = complex)
-eps_eff = np.zeros_like(freq)
-
-for idx, el in enumerate(freq):
-    k0[idx], kc[idx], gamma[idx], eps_eff[idx] = compute_kc(a, b, zz, zt, m, n, el)
-    
 fig, ax = plt.subplots()
-ax.plot(k0*a, eps_eff, marker = "s", markevery=20, label =f"TE{n}0")
-ax.set_ylabel('$\epsilon_{eff}$')
-ax.set_xlabel('$k_0 a$')
 
-ax.set_ylim(0, 5)
+modes = [[1,0], [2,0], [1,1]]
 
-ax.grid(True, color='gray', linestyle=':')
-ax.legend()
-
+for mode in modes:
+    m = mode[1]
+    n = mode[0]
+    
+    freq = np.geomspace(1e6, 2e10, 1001)
+    k0 = np.zeros_like(freq)
+    kc = np.zeros_like(freq)
+    gamma = np.zeros_like(freq, dtype = complex)
+    eps_eff = np.zeros_like(freq)
+    
+    for idx, el in enumerate(freq):
+        k0[idx], kc[idx], gamma[idx], eps_eff[idx] = compute_kc(a, b, zz, zt, m, n, el)
+    
+    # The analytical dispersion equation for a rectangular waveguide
+    ky_a= m*np.pi/b
+    kx_a= n*np.pi/a
+    
+    omega = 2*np.pi*freq
+    k0_a = omega*np.sqrt(sc.epsilon_0*sc.mu_0)
+    kc_a = np.ones_like(freq)*np.sqrt(kx_a**2 + ky_a**2)
+    
+    gamma_a = np.sqrt(kc_a**2 - k0_a**2, dtype=complex)
+    eps_eff_a = (np.imag(gamma_a)/k0_a)**2
+    
+    
+    ax.plot(k0*a, eps_eff, marker = "s", markevery=20, label =f"TE{n}{m}")
+    ax.plot(k0_a*a, eps_eff_a, marker = "s", markevery=30, label =f"TE{n}{m} Analytical")
+    ax.set_ylabel('$\epsilon_{eff}$')
+    ax.set_xlabel('$k_0 a$')
+    
+    ax.set_ylim(0, 5)
+    
+    ax.grid(True, color='gray', linestyle=':')
+    ax.legend()
+    
 plt.show()
 
 #%% Fields computation
@@ -142,7 +144,7 @@ def solution(U):
     # extract the eigenvector (column) associated with the minimum eigenvalue
     return e_vecs[:, np.argmin(e_vals)]     
 
-def compute_fields(a, b, zz, zt, m, n, freq):
+def compute_fields(a, b, zz, zt, m, n, freq, flag=True):
     
     k0, kc, gamma, eps_eff = compute_kc(a, b, zz, zt, m, n, freq)
 
@@ -160,6 +162,16 @@ def compute_fields(a, b, zz, zt, m, n, freq):
     
     # This is a vector [Gamma+ Gamma- Psi+ Psi-]
     [Gammap, Gammam, Psip, Psim] = solution(M)
+    
+    try:
+        alpha = Psim/Gammam
+    except ZeroDivisionError:
+        alpha = float('inf')
+    
+    try:
+        beta = Gammam/Psim
+    except ZeroDivisionError:
+        beta = float('inf')
     
     # Compute all the fields:
     # We still need the dependace from z
@@ -182,26 +194,30 @@ def compute_fields(a, b, zz, zt, m, n, freq):
     
         return [Ex, Ey, Ez, Hx, Hy, Hz]
     
-    # Generate a grid of x and y values
-    x_values = np.linspace(0, a, 100)  # 100 points between 0 and a
-    y_values = np.linspace(0, b, 100)  # 100 points between 0 and b
+    if flag==True:
+        # Generate a grid of x and y values
+        x_values = np.linspace(0, a, 100)  # 100 points between 0 and a
+        y_values = np.linspace(0, b, 100)  # 100 points between 0 and b
+        
+        # Create a meshgrid from x and y values
+        x_mesh, y_mesh = np.meshgrid(x_values, y_values)
+        
+        # Evaluate the function for each combination of x and y
+        fields = compute(x_mesh, y_mesh)
+        mesh = [x_values, y_values]
     
-    # Create a meshgrid from x and y values
-    x_mesh, y_mesh = np.meshgrid(x_values, y_values)
-    
-    # Evaluate the function for each combination of x and y
-    fields = compute(x_mesh, y_mesh)
-    mesh = [x_values, y_values]
+        return [fields, mesh, alpha, beta]
+    else:
+        return [alpha, beta]
 
-    return [fields, mesh]
-
+#%% Testing field maps
 # Create a subplot with contourf
 fig, ax = plt.subplots(2,3, figsize=(10, 5))
 
 labels = ["Ex", "Ey", "Ez", "Hx", "Hy", "Hz"]
 
 freq = 10e9
-A = compute_fields(a, b, zz, zt, m, n, freq)
+A = compute_fields(a=a, b=b, zz=0, zt=0, m=1, n=1, freq=20e9)
 
 for idx, el in enumerate(labels):
     row = idx // 3  # Determine the row (0 or 1)
@@ -219,6 +235,117 @@ for idx, el in enumerate(labels):
 
 # Set a title for the whole figure
 fig.suptitle(f'TE{n}{m}, WR90', fontsize=16)
+
+# Show the plot
+plt.tight_layout()
+plt.show()
+
+"""
+#%% Mode checking example
+
+m = 0
+n = 1 # nth zero of f(kc)
+
+freq = np.geomspace(1e6, 2e10, 1001)
+k0 = np.zeros_like(freq)
+kc = np.zeros_like(freq)
+gamma = np.zeros_like(freq, dtype = complex)
+eps_eff = np.zeros_like(freq)
+alpha = np.zeros_like(freq, dtype = complex)
+beta = np.zeros_like(freq, dtype = complex)
+
+for idx, el in enumerate(freq):
+    k0[idx], kc[idx], gamma[idx], eps_eff[idx] = compute_kc(a, b, zz, zt, m, n, el)
+    alpha[idx], beta[idx] = compute_fields(a, b, zz, zt, m, n, el, flag=False)
+
+
+fig, ax = plt.subplots()
+ax.plot(k0*a, eps_eff, marker = "s", markevery=20, label =f"TE{n}0")
+ax.set_ylabel('$\epsilon_{eff}$')
+ax.set_xlabel('$k_0 a$')
+
+ax.set_ylim(0, 5)
+
+ax.grid(True, color='gray', linestyle=':')
+ax.legend()
+
+plt.show()
+
+# Modify the eq. multipy by (1-X)**2
+# I have to actually understand first if the mode is a TE or TM or HE or EH to 
+# then name them in terms of n
+# Check all modes now (all, go up to some orders)
+"""
+
+# The analytical dispersion equation for a rectangular waveguide
+
+def compute_fields_aTE(a, b, m, n, freq, flag="TE"):
+
+    ky= m*np.pi/b
+    kx= n*np.pi/a
+    
+    k0 = 2*np.pi*freq*np.sqrt(sc.epsilon_0*sc.mu_0)
+    kc = np.sqrt(kx**2 + ky**2)
+    
+    gamma_ = np.sqrt(k0**2 - kc**2, dtype=complex)
+    Z0 = np.sqrt(sc.mu_0/sc.epsilon_0)
+    
+    labels = ["Ex", "Ey", "Ez", "Hx", "Hy", "Hz"]
+    
+    if flag=="TE":
+        def compute_fieldsTE(x,y):
+            
+            Hz = np.cos(kx*x)*np.cos(ky*y)
+            Ez = 0*x*y
+            
+            Hx = 1j*gamma_*kx/kc**2*np.sin(kx*x)*np.cos(ky*y)
+            Hy = 1j*gamma_*ky/kc**2*np.cos(kx*x)*np.sin(ky*y)
+            
+            Ex = k0/gamma_*Z0*Hy
+            Ey = -k0/gamma_*Z0*Hx
+        
+            return [Ex, Ey, Ez, Hx, Hy, Hz]
+    
+    
+    # Generate a grid of x and y values
+    x_values = np.linspace(0, a, 100)  # 100 points between 0 and a
+    y_values = np.linspace(0, b, 100)  # 100 points between 0 and b
+    
+    # Create a meshgrid from x and y values
+    x_mesh, y_mesh = np.meshgrid(x_values, y_values)
+    
+    # Evaluate the function for each combination of x and y
+    fields = compute_fieldsTE(x_mesh, y_mesh)
+    mesh = [x_values, y_values]
+
+    return [fields, mesh]
+
+# Create a subplot with contourf
+fig, ax = plt.subplots(2,3, figsize=(10, 5))
+
+labels = ["Ex", "Ey", "Ez", "Hx", "Hy", "Hz"]
+
+freq = 10e9
+m = 1
+n = 1
+A = compute_fields_aTE(a=a, b=b, m=m, n=n, freq=10e9)
+
+for idx, el in enumerate(labels):
+    row = idx // 3  # Determine the row (0 or 1)
+    col = idx % 3   # Determine the column (0, 1, or 2)
+
+    contour = ax[row, col].contourf(A[1][0], A[1][1], np.abs(A[0][idx]), cmap='viridis', levels=100)
+    fig.colorbar(contour, ax=ax[row, col])
+
+    # Add labels and title
+    ax[row, col].set_xlabel('x [m]')
+    ax[row, col].set_ylabel('y [m]')
+    ax[row, col].set_title(el)
+
+    ax[row, col].set_aspect('equal')
+
+# Set a title for the whole figure
+fig.suptitle(f'TE{n}{m} analytical, WR90', fontsize=16)
 
 # Show the plot
 plt.tight_layout()
