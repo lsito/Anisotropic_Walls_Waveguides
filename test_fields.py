8aff7e1447d09be19416eaacbc2e6d5fed59876a
lsito <leonardo.sito@cern.ch>
2024-02-21 10:28:10 +0100

    Phase Issue Test

--- test_fields.py
+++ test_fields.py
@@ -10,7 +10,7 @@ from scipy.signal import find_peaks
 import scipy.constants as sc
 
 # Plot in separate window
-%matplotlib qt
+#%matplotlib qt
 
 #%% Test Field distributions with analytical eq from textbook
 
@@ -25,13 +25,13 @@ b=10.16e-3
 WR90 = anwg.Waveguide(a=a, b=b, zz=0, zt=0, sigma=58e6)
 Dispersion_WR90 = anwg.DispersionCurve(waveguide=WR90, m=0, n=1, freq=freq)
 Fields_WR90 = anwg.Fields(waveguide=WR90, dispersionCurve=Dispersion_WR90, Nx=100, Ny=100)
-Fields_WR90.compute_fields_analytical(mode='TE')
+#Fields_WR90.compute_fields_analytical(mode='TE')
 Fields_WR90.compute_fields()
 
-A = [Fields_WR90.Ex, Fields_WR90.Ey, Fields_WR90.Ez, 
-     Fields_WR90.Hx, Fields_WR90.Hy, Fields_WR90.Hz]
+A = [-Fields_WR90.Ex, -Fields_WR90.Ey, -Fields_WR90.Ez, 
+     -Fields_WR90.Hx, -Fields_WR90.Hy, -Fields_WR90.Hz]
 
-norm_factor = np.amax(np.amax(np.abs(A)))
+norm_factor = np.amax(np.amax(np.imag(A)))
 
 for idx, el in enumerate(labels):
     row = idx // 3  # Determine the row (0 or 1)
@@ -48,7 +48,48 @@ for idx, el in enumerate(labels):
     ax[row, col].set_aspect('equal')
 
 # Set a title for the whole figure
-fig.suptitle(f'TE{1}{0}, WR90', fontsize=16)
+fig.suptitle(f'Mode Theory TE{1}{0}, WR90', fontsize=16)
+
+# Show the plot
+plt.tight_layout()
+plt.show()
+
+#%% Test Field distributions with analytical eq from textbook
+
+fig, ax = plt.subplots(2,3, figsize=(10, 5))
+
+labels = ["Ex", "Ey", "Ez", "Hx", "Hy", "Hz"]
+
+freq = 15e9
+a=22.86e-3
+b=10.16e-3
+
+WR90 = anwg.Waveguide(a=a, b=b, zz=0, zt=0, sigma=58e6)
+Dispersion_WR90 = anwg.DispersionCurve(waveguide=WR90, m=0, n=1, freq=freq)
+Fields_WR90 = anwg.Fields(waveguide=WR90, dispersionCurve=Dispersion_WR90, Nx=100, Ny=100)
+Fields_WR90.compute_fields_analytical(mode='TE')
+
+A = [Fields_WR90.Ex, Fields_WR90.Ey, Fields_WR90.Ez, 
+     Fields_WR90.Hx, Fields_WR90.Hy, Fields_WR90.Hz]
+
+norm_factor = np.amax(np.amax(np.imag(A)))
+
+for idx, el in enumerate(labels):
+    row = idx // 3  # Determine the row (0 or 1)
+    col = idx % 3   # Determine the column (0, 1, or 2)
+
+    contour = ax[row, col].contourf(Fields_WR90.x, Fields_WR90.y, np.imag(A[idx])/norm_factor, cmap='viridis', levels=100)
+    fig.colorbar(contour, ax=ax[row, col])
+
+    # Add labels and title
+    ax[row, col].set_xlabel('x [m]')
+    ax[row, col].set_ylabel('y [m]')
+    ax[row, col].set_title(el)
+
+    ax[row, col].set_aspect('equal')
+
+# Set a title for the whole figure
+fig.suptitle(f'Analytical TE{1}{0}, WR90', fontsize=16)
 
 # Show the plot
 plt.tight_layout()
